9f28b6b19283d27d6941cc712024e69195186693
soberanoJr <tonysoberano@gmail.com>
2022-05-13 10:55:36 -0300

    paddle

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from turtle import Screen
+from paddle import Paddle
 
+
+player_1 = Paddle((-350, 0))
+player_2 = Paddle((350, 0))
 
 screen = Screen()
 screen.title("PONG")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
+screen.tracer(0)
+screen.listen()
+screen.onkey(player_1.up, "q")
+screen.onkey(player_1.down, "a")
+screen.onkey(player_2.up, "p")
+screen.onkey(player_2.down, "l")
+
+play = True
+
+while play:
+
+    screen.update()
 
 screen.exitonclick()
